4eb6a5a807cb5bcf51a3599d19bee67ecbb19e0d
Mingz <n_minghao28@hotmail.com>
2024-06-08 16:43:12 +0800

    add getAccounts api with error handling

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
-
 export default function Home() {
   return (
-    <div className=""></div>
+    <div className="">
+      Homepage
+    </div>
   );
 }
--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -3,29 +3,24 @@ import { zValidator } from '@hono/zod-validator';
 import { Hono } from 'hono';
 import { handle } from 'hono/vercel';
 import { clerkMiddleware, getAuth } from '@hono/clerk-auth';
+import accounts from './accounts';
+import { HTTPException } from 'hono/http-exception';
 
 export const runtime = 'edge';
 
 const app = new Hono().basePath('/api')
 
-app
-    .get('/hello',
-        clerkMiddleware(),
-        (c) => {
-            const auth = getAuth(c);
+app.onError((err, c) => {
+    if (err instanceof HTTPException) {
+        return err.getResponse();
+    }
 
-            if (!auth?.userId) {
-                return c.json({
-                    error: "Unauthorized",
-                });
-            }
-
-            return c.json({
-                message: 'Hello Next.js!',
-                userId: auth.userId,
-            });
-        }
-    )
+    return c.json({ error: err.message }, 500);
+})
+const routes = app
+    .route("/accounts", accounts);
 
 export const GET = handle(app);
 export const POST = handle(app);
+
+export type AppType = typeof routes;
