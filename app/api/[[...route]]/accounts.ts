ff4bbd234e3147b5240f61fb2cda55153334547a
Mingz <n_minghao28@hotmail.com>
2024-06-09 16:45:17 +0800

    add post accounts api

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,6 +1,8 @@
 import { db } from "@/db/drizzle";
-import { accounts } from "@/db/schema";
+import { accounts, insertAccountSchema } from "@/db/schema";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+import { createId } from "@paralleldrive/cuid2";
+import { zValidator } from "@hono/zod-validator";
 import { eq } from "drizzle-orm";
 import { Hono } from "hono";
 
@@ -23,6 +25,28 @@ const app = new Hono()
                 .where(eq(accounts.userId, auth.userId));
 
             return c.json({ data });
-    });
+    })
+    .post("/",
+        clerkMiddleware(),
+        zValidator("json", insertAccountSchema.pick({
+            name: true,
+        })),
+        async (c) => {
+            const auth = getAuth(c);
+            const values = c.req.valid("json");
+
+            if (!auth?.userId) {
+                return c.json({ error: "Unauthorized" }, 401);
+            }
+
+            // Can destructure because only one row is inserted
+            const [data] = await db.insert(accounts).values({
+                id: createId(),
+                userId: auth.userId,
+                ...values,
+            }).returning();
+
+            return c.json({ data });
+        })
 
 export default app;
