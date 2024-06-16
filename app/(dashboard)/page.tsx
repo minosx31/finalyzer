e917e9f2733771b202758a26065d8c7f75486ba9
Mingz <n_minghao28@hotmail.com>
2024-06-16 21:45:14 +0800

    add basic account creation form

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,15 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
+
 export default function Home() {
+  const { isOpen, onOpen, onClose } = useNewAccount();
   return (
     <div className="">
-      Homepage
+      <Button onClick={onOpen}>
+        Add an account
+      </Button>
     </div>
   );
 }
