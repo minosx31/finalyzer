7752f7410deae9fa2adfb47d9c33370c48435f2d
Mingz <n_minghao28@hotmail.com>
2024-07-12 16:54:21 +0800

    add confirmation dialog for delete

--- components/ui/data-table.tsx
+++ components/ui/data-table.tsx
@@ -23,6 +23,7 @@ import {
 } from "@/components/ui/table"
 import { Button } from "./button"
 import { Input } from "./input"
+import { useConfirm } from "@/hooks/use-confirm"
 import { Trash } from "lucide-react"
 
 interface DataTableProps<TData, TValue> {
@@ -40,6 +41,11 @@ export function DataTable<TData, TValue>({
   onDelete,
   disabled,
 }: DataTableProps<TData, TValue>) {
+  const [ConfirmDialog, confirm] = useConfirm(
+    "Are you sure?",
+    "You are about to delete the selected row(s). This action cannot be undone."
+  );
+
   const [sorting, setSorting] = React.useState<SortingState>([]);
   const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>([]);
   const [rowSelection, setRowSelection] = React.useState({});
@@ -63,6 +69,7 @@ export function DataTable<TData, TValue>({
 
   return (
     <div>
+      <ConfirmDialog />
       <div className="flex items-center py-4">
         <Input
           placeholder={`Filter ${filterKey}...`}
@@ -78,9 +85,12 @@ export function DataTable<TData, TValue>({
             size="sm"
             variant="outline"
             className="ml-auto font-normal text-xs"
-            onClick={() => {
-              onDelete(table.getFilteredSelectedRowModel().rows);
-              table.resetRowSelection();
+            onClick={async () => {
+              const ok = await confirm();
+              if (ok) {
+                onDelete(table.getFilteredSelectedRowModel().rows);
+                table.resetRowSelection();
+              }
             }}
           >
             <Trash className="size-4 mr-2" />
