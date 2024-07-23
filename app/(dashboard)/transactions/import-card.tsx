cd67dac3070479dd5a83f746a30a0346c554b69f
Mingz <n_minghao28@hotmail.com>
2024-07-23 14:37:03 +0800

    add csv import functionalty

---
+++ app/(dashboard)/transactions/import-card.tsx
@@ -0,0 +1,136 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { useState } from "react";
+import { ImportTable } from "./import-table";
+import { convertAmountToMiliUnits } from "@/lib/utils";
+import { format, parse } from "date-fns";
+
+const dateFormat = "yyyy-MM-dd HH:mm:ss";
+const outputFormat = "yyyy-MM-dd";
+
+const requiredOptions = [
+    "amount",
+    "date",
+    "payee",
+];
+
+interface SelectedColumnState {
+    [key: string]: string | null;
+};
+
+type Props = {
+    data: string[][];
+    onCancel: () => void;
+    onSubmit: (data: any) => void;
+};
+
+export const ImportCard = ({
+    data,
+    onCancel,
+    onSubmit,
+}: Props) => {
+    const [selectedColumns, setSelectedColumns] = useState<SelectedColumnState>({})
+    const headers = data[0];
+    const body = data.slice(1);
+
+    const onTableHeadSelectChange = (
+        columnIndex: number,
+        value: string | null
+    ) => {
+        setSelectedColumns((prev) => {
+            const newSelectedColumns = { ...prev };
+
+            for (const key in newSelectedColumns) {
+                if (newSelectedColumns[key] === value) {
+                    newSelectedColumns[key] = null;
+                }
+            }
+
+            if (value === "skip") {
+                value = null;
+            }
+
+            newSelectedColumns[`column_${columnIndex}`] = value;
+            return newSelectedColumns;
+        })
+    }
+
+    const progress = Object.values(selectedColumns).filter(Boolean).length;
+
+    const handleContinue = () => {
+        const getColumnIndex = (column: string) => {
+            return column.split("_")[1];
+        };
+
+        const mappedData = {
+            headers: headers.map((_header, index) => {
+                const columnIndex = getColumnIndex(`column_${index}`);
+                return selectedColumns[`column_${columnIndex}`] || null;
+            }),
+            body: body.map((row) => {
+                const transformedRow = row.map((cell, index) => {
+                    const columnIndex = getColumnIndex(`column_${index}`);
+                    return selectedColumns[`column_${columnIndex}`] ? cell : null;
+                });
+
+                return transformedRow.every((item) => item === null) ? [] : transformedRow;
+            }).filter((row) => row.length > 0),
+        };
+
+        const arrayOfData = mappedData.body.map((row) => {
+            return row.reduce((acc: any, cell, index) => {
+                const header = mappedData.headers[index];
+                if (header !== null) {
+                    acc[header] = cell;
+                }
+
+                return acc;
+            }, {});
+        });
+
+        const formattedData = arrayOfData.map((item) => ({
+            ...item,
+            amount: convertAmountToMiliUnits(item.amount),
+            date: format(parse(item.date, dateFormat, new Date()), outputFormat),
+        }));
+
+        onSubmit(formattedData);
+    }
+
+    return (
+        <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+            <Card className="border-none drop-shadow-md">
+                <CardHeader className="gap-y-2 md:flex-row md:items-center md:justify-between">
+                    <CardTitle className="text-xl line-clamp-1">
+                        Import Transactions
+                    </CardTitle>
+                    <div className="flex flex-col md:flex-row gap-y-2 items-center gap-x-2">
+                        <Button
+                            size="sm"
+                            onClick={onCancel}
+                            className="w-full md:w-auto"
+                        >
+                            Cancel
+                        </Button>
+                        <Button
+                            size="sm"
+                            onClick={handleContinue}
+                            disabled={progress < requiredOptions.length}
+                            className="w-full md:w-auto"
+                        >
+                            Continue ({progress} / {requiredOptions.length})
+                        </Button>
+                    </div>
+                </CardHeader>
+                <CardContent>
+                    <ImportTable
+                        headers={headers}
+                        body={body}
+                        selectedColumns={selectedColumns}
+                        onTableHeadSelectChange={onTableHeadSelectChange}
+                    />
+                </CardContent>
+            </Card>
+        </div>
+    )
+}
--- app/(dashboard)/transactions/page.tsx
+++ app/(dashboard)/transactions/page.tsx
@@ -8,8 +8,33 @@ import { DataTable } from "@/components/ui/data-table";
 import { useGetTransactions } from "@/features/transactions/api/use-get-transactions";
 import { Skeleton } from "@/components/ui/skeleton";
 import { useBulkDeleteTransactions } from "@/features/transactions/api/use-bulk-delete-transactions";
+import { useState } from "react";
+import { UploadButton } from "./upload-button";
+import { ImportCard } from "./import-card";
+
+enum VARIANTS {
+    LIST = "LIST",
+    IMPORT = "IMPORT",
+};
+
+const INITIAL_IMPORT_RESULTS = {
+    data: [],
+    errors: [],
+    meta: {},
+};
 
 const TransactionsPage = () => {
+    const [variant, setVariant] = useState<VARIANTS>(VARIANTS.LIST);
+    const [importResults, setImportResults] = useState<typeof INITIAL_IMPORT_RESULTS>(INITIAL_IMPORT_RESULTS);
+    const onUpload = (results: typeof INITIAL_IMPORT_RESULTS) => {
+        setImportResults(results);
+        setVariant(VARIANTS.IMPORT);
+    };
+    const onCancelImport = () => {
+        setImportResults(INITIAL_IMPORT_RESULTS);
+        setVariant(VARIANTS.LIST);
+    };
+
     const newTransaction = useNewTransaction();
     const deleteTransactions = useBulkDeleteTransactions();
     const transactionsQuery = useGetTransactions();
@@ -35,6 +60,18 @@ const TransactionsPage = () => {
         )
     }
 
+    if (variant === VARIANTS.IMPORT) { 
+        return (
+            <>
+                <ImportCard
+                    data={importResults.data}
+                    onCancel={onCancelImport}
+                    onSubmit={() => {}}
+                />
+            </>
+        )
+    }
+
     return (
         <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
             <Card className="border-none drop-shadow-md">
@@ -42,10 +79,19 @@ const TransactionsPage = () => {
                     <CardTitle className="text-xl line-clamp-1">
                         Transactions Page
                     </CardTitle>
-                    <Button size="sm" onClick={newTransaction.onOpen}>
-                        <Plus className="size-4 mr-2" />
-                        Add New
-                    </Button>
+                    <div className="flex flex-col md:flex-row gap-y-2 items-center gap-x-2">
+                        <Button
+                            size="sm"
+                            onClick={newTransaction.onOpen}
+                            className="w-full md:w-auto"
+                        >
+                            <Plus className="size-4 mr-2" />
+                            Add New
+                        </Button>
+                        <UploadButton
+                            onUpload={onUpload}
+                        />
+                    </div>
                 </CardHeader>
                 <CardContent>
                     <DataTable
