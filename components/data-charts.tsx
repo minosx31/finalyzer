8e04f38814e69f1675fa82014f3c1bbf79b17445
Mingz <n_minghao28@hotmail.com>
2024-07-30 22:11:25 +0800

    add different types of chart in dashboard page

---
+++ components/data-charts.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useGetSummary } from "@/features/summary/api/use-get-summary";
+import { Chart } from "@/components/chart";
+
+export const DataCharts = () => {
+    const { data, isLoading } = useGetSummary();
+
+    if (isLoading) {
+        return (
+            <div>
+                Loading....
+            </div>
+        )
+    }
+
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-6 gap-8">
+            <div className="col-span-1 md:col-span-3 xl:col-span-4">
+                <Chart data={data?.days} />
+            </div>
+
+        </div>
+    )
+}
