10ee2a953296176fd1320868224a95c80913ec4b
minosx31 <n_minghao28@hotmail.com>
2025-08-20 15:46:19 +0800

    Add prop

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider afterSignOutUrl="/">
-      <html lang="en">
+      <html lang="en" suppressHydrationWarning>
         <body className={inter.className}>
           <QueryProvider>
             <ThemeProvider
